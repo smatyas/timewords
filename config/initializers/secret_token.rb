85275453ac828a4c24e321a2ed98d6f5306d7bdc
Mátyás Somfai <somfai.matyas@gmail.com>
2012-02-21 20:27:46 +0100

    initial import, base rails system without database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timewords::Application.config.secret_token = 'd59101381591edb17593f254448c3a9aceb20f843ed86013ff8608bf528a530790837e1602f7b911a24e4f87133b8520ca82e3314e21e81ec276483689da135c'
